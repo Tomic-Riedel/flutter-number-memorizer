a60f1c16c6b3d081b194c604906c1807d65debe4
Tomic-Riedel <tomicriedel07@gmail.com>
2022-06-25 10:47:49 +0200

    Implementing automatic scroll

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "2b5e87c7d415a44121b31c12ebac49af",
-"index.html": "c4d8e38caa5094d7809171f933959879",
-"/": "c4d8e38caa5094d7809171f933959879",
-"main.dart.js": "503e1252d8197b09494b49cb71b9eed7",
+"index.html": "56622f9457aa8fa1bb3fd50175161f7f",
+"/": "56622f9457aa8fa1bb3fd50175161f7f",
+"main.dart.js": "25b0b150f328a30d234a84b5c4c234c7",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -15,7 +15,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "04c2f36e785408ca8c0e2862226ae6f3",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "a232ecb156857ec45dc0b2a7fc69d6ff",
+"assets/NOTICES": "dbd1a51f3d97d4f75eebc4b68c42992c",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
